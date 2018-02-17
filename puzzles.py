5be60ed1b003818692f51e0b4746effa76612250
ben hu <benhu@envoy.com>
2018-02-16 23:45:29 -0800

    initial

---
+++ puzzles.py
@@ -0,0 +1,48 @@
+# usr/bin/env python3
+
+import io
+import os
+import requests
+from bs4 import BeautifulSoup as bs
+
+import scrapy
+#import urllib2
+
+# 2010: 6 - 28
+
+BASE_URL = 'https://www.collegepuzzlechallenge.com/'
+results = {} # {'puzzle', 'solution'}
+
+def main():
+	years = list(range(2010, 2018))
+
+	for year in years:
+		url = BASE_URL + 'Archive/' + str(year) + '/Default.aspx'
+
+		resp = requests.get(url)
+		
+		#print (resp.content)
+		print(resp.status_code)
+		if(resp.status_code < 400):
+			soup = bs(resp.text, 'lxml')
+
+			for link in soup.find_all('a', class_='PdfFile'):
+				pdf = link['href']
+				print(pdf[6:])
+				if 'solution' not in pdf:
+					results[pdf[6:]] =  pdf[6:] + '&view=solution'
+
+
+	output = open('puzzles.csv','w')
+
+	for k, v in results.items():
+		output.write(BASE_URL+k)
+		output.write(',')
+		output.write(BASE_URL+v)
+		output.write('\n')
+
+
+
+if __name__ == '__main__':
+
+	main()
